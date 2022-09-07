ad63d91e757c0b4fdd97d59b7ddf2f7e25c47126
Keks <keks@htmlacademy.ru>
2022-09-07 10:10:21 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/script.js
@@ -0,0 +1,16 @@
+
+if (document.querySelector('.no-js')) {
+  document.querySelector('.no-js').classList.remove('no-js');
+}
+
+// Скрипт для меню
+const menuBtn = document.querySelector('.navigation__btn');
+const menuNav = document.querySelector('.navigation');
+
+
+if (menuBtn) {
+  menuBtn.addEventListener('click', function (evt) {
+    evt.preventDefault;
+    menuNav.classList.toggle('navigation--opened');
+  });
+}
